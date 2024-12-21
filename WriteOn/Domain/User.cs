bc645a6a40823b5f6e3b012aa4d01730950f41cc
Philip Zadeh <philezad@gmail.com>
2024-12-21 21:04:16 +0100

    Added user domain

---
+++ WriteOn/Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class User
+{
+    public required int Id { get; set; }
+    public required string AccountName { get; set; }
+    public required string PlainPassword { get; set; }
+}
