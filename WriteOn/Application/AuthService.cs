fe284dcb1d0d8ffdff8a345eb22ceb352d5e233a
Philip Zadeh <philezad@gmail.com>
2024-12-21 21:13:32 +0100

    Made AuthService and interface

---
+++ WriteOn/Application/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs.Create;
+using Application.DTOs.Request;
+using Application.DTOs.Response;
+using Application.Interfaces;
+
+namespace Application;
+
+public class AuthService : IAuthService
+{
+    public bool Register(RegisterDto registerDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public LoginResponse Login(LoginDto loginDto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ WriteOn/Application/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs.Create;
+using Application.DTOs.Request;
+using Application.DTOs.Response;
+
+namespace Application.Interfaces;
+
+public interface IAuthService
+{
+    public bool Register(RegisterDto registerDto);
+    public LoginResponse Login(LoginDto loginDto);
+}
