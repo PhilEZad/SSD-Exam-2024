bb8537390dae07376725da4153e6ea734b92f3a0
Philip Zadeh <philezad@gmail.com>
2024-12-21 23:47:57 +0100

    Updated backend name to fit with frontend

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        try
+        {
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        try
+        {
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
---
+++ Backend/API/Controllers/NoteController.cs
@@ -0,0 +1,72 @@
+﻿using Application.DTOs.Create;
+using Application.DTOs.Update;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NoteController : ControllerBase
+{
+    private readonly INoteService _noteService;
+    
+    public NoteController(INoteService noteService)
+    {
+        _noteService = noteService;
+    }
+
+    [HttpPost]
+    public IActionResult AddNoteAsync([FromBody] NoteCreate note)
+    {
+        try
+        {
+            return Ok(_noteService.Create(note));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult ReadNoteById([FromRoute] int id)
+    {
+        try
+        {
+            return Ok(_noteService.ReadById(id));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPatch]
+    public IActionResult UpdateNoteAsync([FromBody] NoteUpdate note)
+    {
+        try
+        {
+            return Ok(_noteService.Update(note));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult GetNoteById([FromRoute] int id)
+    {
+        try
+        {
+            return Ok(_noteService.Delete(id));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+}
