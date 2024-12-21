ad7d9daf5db85ab14a352271d66b7a610a996aae
Philip Zadeh <philezad@gmail.com>
2024-12-21 03:04:11 +0100

    Create StickyNoteValidator.cs

---
+++ WriteOn/Application/Validator/StickyNoteValidator.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using FluentValidation;
+
+namespace Application.Validator;
+
+public class StickyNoteValidator : AbstractValidator<StickyNote>
+{
+    
+    
+    public StickyNoteValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+        
+        RuleFor(x => x.Id).NotNull().LessThanOrEqualTo(0).WithMessage("Id cannot be null");
+        RuleFor(x => x.Id).LessThanOrEqualTo(0).WithMessage("Id cannot be 0");
+        
+        RuleFor(x => x.Title).NotNull().NotEmpty().WithMessage("Title cannot be null");
+        RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required");
+        
+        RuleFor(x => x.Content).NotNull().WithMessage("Content cannot be null");
+    }
+}
