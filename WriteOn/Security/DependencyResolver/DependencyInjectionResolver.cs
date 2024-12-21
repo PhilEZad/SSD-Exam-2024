b5d60e2c047f68c766b2314ed9ccd5f5dec2e4cb
Philip Zadeh <philezad@gmail.com>
2024-12-21 23:27:51 +0100

    Update DependencyInjectionResolver.cs

--- WriteOn/Security/DependencyResolver/DependencyInjectionResolver.cs
+++ WriteOn/Security/DependencyResolver/DependencyInjectionResolver.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Security.DependencyResolver;
 
@@ -6,6 +7,6 @@ public static class DependencyInjectionResolver
 {
     public static void RegisterSecurity(this IServiceCollection services)
     {
-        
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
     }
 }
