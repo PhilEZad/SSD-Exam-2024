6d151dd547678529f09baa9690a529d5f48beed1
Philip Zadeh <philezad@gmail.com>
2024-12-22 12:19:31 +0100

    Made validator for user

--- Backend/Application/AuthService.cs
+++ Backend/Application/AuthService.cs
@@ -2,11 +2,20 @@
 using Application.DTOs.Request;
 using Application.DTOs.Response;
 using Application.Interfaces;
+using Domain;
+using FluentValidation;
 
 namespace Application;
 
 public class AuthService : IAuthService
 {
+    private readonly IValidator<User> _validator;
+
+    public AuthService(IValidator<User> validator)
+    {
+        _validator = validator;
+    }
+    
     public bool Register(RegisterDto registerDto)
     {
         throw new NotImplementedException();
---
+++ Backend/Application/Validator/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using FluentValidation;
+
+namespace Application.Validator;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+        
+        RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id must be greater than 0");
+        
+        RuleFor(x => x.Username).NotEmpty().WithMessage("Username cannot be empty");
+        RuleFor(x => x.PlainPassword).NotEmpty().WithMessage("Password cannot be empty");
+    }
+}
