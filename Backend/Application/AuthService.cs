5a3117438f9ddbf986932711ae7bfea05d72234d
Philip Zadeh <philezad@gmail.com>
2024-12-22 12:25:17 +0100

    Update AuthService.cs

--- Backend/Application/AuthService.cs
+++ Backend/Application/AuthService.cs
@@ -10,10 +10,16 @@ namespace Application;
 public class AuthService : IAuthService
 {
     private readonly IValidator<User> _validator;
+    private readonly IJwtProvider _jwtProvider;
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly IAuthRepository _authRepository;
 
-    public AuthService(IValidator<User> validator)
+    public AuthService(IValidator<User> validator, IJwtProvider jwtProvider, IPasswordHasher passwordHasher, IAuthRepository authRepository)
     {
         _validator = validator;
+        _jwtProvider = jwtProvider;
+        _passwordHasher = passwordHasher;
+        _authRepository = authRepository;
     }
     
     public bool Register(RegisterDto registerDto)
