0ede008eef5ae42ea567a84c5b93d6747766f930
RSS <90904706+sandbxk@users.noreply.github.com>
2024-12-25 16:13:23 +0100

    Login and register routes, masonry layout

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 import {LoginPageComponent} from './login-page/login-page.component';
 import {RegistrationPageComponent} from './registration-page/registration-page.component';
+import {homeGuard} from '../services/guards/home-guard';
+import {loginGuard} from '../services/guards/login-guard';
+import {registerGuard} from '../services/guards/register-guard';
 
 export const routes: Routes = [
   // Add your routes here
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', pathMatch: 'full', component: HomePageComponent},
-  {path: 'login', pathMatch: 'full', component: LoginPageComponent},
-  {path: 'register', pathMatch: 'full', component: RegistrationPageComponent},
+  {path: 'home', pathMatch: 'full', component: HomePageComponent, canActivate: [homeGuard]},
+  {path: 'login', pathMatch: 'full', component: LoginPageComponent, canActivate: []},
+  {path: 'register', pathMatch: 'full', component: RegistrationPageComponent, canActivate: []},
   {path: '**', redirectTo: '/home'},
 
 
